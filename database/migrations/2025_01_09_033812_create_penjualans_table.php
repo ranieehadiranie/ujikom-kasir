b764ad41e905cfe8d060efc1005c8ed47cd13e11
hadiranie <hadiraniebsc@gmail.com>
2025-01-09 11:10:51 +0700

    setting migration kasir-laravel

--- database/migrations/2025_01_09_033812_create_penjualans_table.php
+++ database/migrations/2025_01_09_033812_create_penjualans_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('penjualans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role',['admin','petugas'])->default('petugas');
-            $table->rememberToken();
+            $table->date('TanggalPenjualan');
+            $table->decimal('TotalHarga', 10, 2);
+            $table->unsignedBigInteger('UsersId');
+
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('penjualans');
     }
 };
--- database/migrations/2025_01_09_040207_create_produks_table.php
+++ database/migrations/2025_01_09_040207_create_produks_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('produks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role',['admin','petugas'])->default('petugas');
-            $table->rememberToken();
+            $table->string('NamaProduk');
+            $table->decimal('Harga', 10, 2);
+            $table->integer('Stok');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('produks');
     }
 };
